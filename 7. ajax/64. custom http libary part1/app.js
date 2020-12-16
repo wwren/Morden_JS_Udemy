ee87f3363b59fb4d565d72a939c75f477e00dee4
wwren <65450330+wwren@users.noreply.github.com>
2020-12-16 22:21:16 +1100

    Update app.js

--- 7. ajax/64. custom http libary part1/app.js
+++ 7. ajax/64. custom http libary part1/app.js
@@ -2,7 +2,7 @@ const http = new easyHTTP;
 
 // Get Posts
 http.get('https://jsonplaceholder.typicode.com/posts', function(err, post){
-  if (err){
+  if (err){ //because when the status returned not 200, callback('Error: ' + this.http.status ); -> a not empty string is evaluated to be true -> so log error message
     console.log(err);
   }
   else {
