de49d9f3d30fbf50c4cc61904f16fdd9241886b3
wwren <65450330+wwren@users.noreply.github.com>
2021-02-14 20:53:36 +1100

    Update github.js
    hide client key

--- 8. api projects/github.js
+++ 8. api projects/github.js
@@ -1,7 +1,7 @@
 class Github {
   constructor() {
-    this.client_id = "2ca54a7063f6b506f39b";
-    this.client_secret = "ba85d6c8490880f958e75f76c5a13581fa8c4ec4";
+    this.client_id = "**";
+    this.client_secret = "**";
   }
 
   async getUser(user) {
